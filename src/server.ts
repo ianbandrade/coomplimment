535efe8718777c2ea962c34aee3ee7f5aff2afe2
ianbandrade <ianbandrade14@gmail.com>
2021-06-28 19:19:23 -0300

    add: cors

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,13 @@ import './database';
 import 'express-async-errors';
 import { router } from '../src/routes/routes';
 import { errorsTreatment } from './middlewares/errorsTreatment';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
-
 app.use(errorsTreatment);
 
 app.listen(3333, () => console.log(`Running server on: http://localhost:3333`));
