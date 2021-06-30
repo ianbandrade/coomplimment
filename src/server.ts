e7bcf2e1a5cff4283811f6273b532337d3ccf515
ianbandrade <ianbandrade14@gmail.com>
2021-06-30 20:30:29 -0300

    add: swagger-ui

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,14 @@ import 'express-async-errors';
 import { router } from '../src/routes/routes';
 import { errorsTreatment } from './middlewares/errorsTreatment';
 import cors from 'cors';
+import { serve, setup } from 'swagger-ui-express';
+import swaggerSetup from '../swagger/swagger.json';
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 app.use(router);
 app.use(errorsTreatment);
+app.use('/docs', serve, setup(swaggerSetup));
 
 app.listen(3333, () => console.log(`Running server on: http://localhost:3333`));
